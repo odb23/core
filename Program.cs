e5b2a4cdcdde056dad05569645799806ea2d7a11
Dami <dami.odulaja@oaperg.com>
2022-09-05 10:59:17 +0100

    Configuring Middleware, Options pattern

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 using Core;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<FruitOptions>(options =>
+{
+    options.Name = "Watermelon";
+});
+
 var app = builder.Build();
 
+app.UseMiddleware<FruitsMiddleware>();
 app.UseMiddleware<MiddleWare>();
 
 app.MapGet("/", () => "Hello World!");
